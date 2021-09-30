7dc4e1d098b3d8bf825358fd76ccf996c1973f2d
Jack Rackham <j.calico.rackham@gmail.com>
2021-09-30 01:10:27 -0500

    agrego eventos de teclado para pantalla de registro

---
+++ proyecto-cliente/proyecto-cliente/CorreoRecuperacionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CorreoRecuperacionViewController.swift
+//  proyecto-cliente
+//
+//  Created by Jack Rackham on 9/30/21.
+//
+
+import UIKit
+
+class CorreoRecuperacionViewController: UIViewController {
+
+    @IBAction func clickCloseKeyboard(_ sender: Any){
+        self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
