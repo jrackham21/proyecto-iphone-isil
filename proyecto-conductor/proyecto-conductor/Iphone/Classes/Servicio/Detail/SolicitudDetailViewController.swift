1745d066523c92ea57b4e78c77421ec039e756e5
Jack Rackham <j.calico.rackham@gmail.com>
2021-12-12 20:38:30 -0500

    termino pantallas de solicitudes

--- proyecto-conductor/proyecto-conductor/Iphone/Classes/Servicio/Detail/SolicitudDetailViewController.swift
+++ proyecto-conductor/proyecto-conductor/Iphone/Classes/Servicio/Detail/SolicitudDetailViewController.swift
@@ -1,22 +1,23 @@
 //
-//  ServicioDetailViewController.swift
+//  SolicitudDetailViewController.swift
 //  proyecto-conductor
 //
-//  Created by Jack Rackham on 12/5/21.
+//  Created by Jack Rackham on 12/12/21.
 //
 
 import UIKit
 
-class ServicioDetailViewController: UIViewController {
+class SolicitudDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
     }
     
     // MARK: - Actions
     @IBAction func clickBtnBack(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
-    
+
 }
