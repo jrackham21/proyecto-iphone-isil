df2273fba0f355ff6710337bb971e3de6f6c2156
sheryl222 <sheryl_g2@outlook.com>
2021-12-15 06:47:33 -0500

    arigatou norvil sempai

--- proyecto-cliente/proyecto-cliente/Iphone/Storyboars/EditarPerfilViewController.swift
+++ proyecto-cliente/proyecto-cliente/Iphone/Storyboars/EditarPerfilViewController.swift
@@ -5,4 +5,36 @@
 //  Created by user191544 on 12/13/21.
 //
 
-import Foundation
+
+import UIKit
+
+class EditarPerfilViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var tfTelefono: UITextField!
+    @IBOutlet weak var imgPerfil: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Actions
+    @IBAction func clickBtnBack(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func tomarFoto(_ sender: Any){
+        
+    }
+    
+    @IBAction func subirFoto(_ sender: Any){
+        
+    }
+    
+    @IBAction func guardar(_ sender: Any){
+        // TODO
+        self.navigationController?.popViewController(animated: true)
+    }
+
+}
