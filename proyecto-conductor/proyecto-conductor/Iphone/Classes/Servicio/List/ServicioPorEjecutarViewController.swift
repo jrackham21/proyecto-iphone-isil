2ef01deac2ff629a09a87d990dd6063f1c0b18cd
Jack Rackham <j.calico.rackham@gmail.com>
2021-12-15 05:06:53 -0500

    lista y detalle desde arrayList

--- proyecto-conductor/proyecto-conductor/Iphone/Classes/Servicio/List/ServicioPorEjecutarViewController.swift
+++ proyecto-conductor/proyecto-conductor/Iphone/Classes/Servicio/List/ServicioPorEjecutarViewController.swift
@@ -12,7 +12,33 @@ class ServicioPorEjecutarViewController: UIViewController {
     
     @IBOutlet private weak var tblServicios: UITableView!
     
-    var arrayServicios = ["sdaf", "234", "vdf", "sadf", "fq3t5", "fd34vd"]
+    let db = Firestore.firestore()
+    
+    var arrayServicios: [(cliente: String, nombre: String, apellido: String, recojo: String, destino: String, hora: String, conductor: String, imagen: String, estado: Int )] =
+        
+        [("easy@gmail.com", "Iris", "Steensma", "here", "there", "now", "Travis", "https://lamanodelextranjero.files.wordpress.com/2013/10/jodie-foster-como-la-prostituta-iris_thumb.jpg", 1),
+         ("kamesenin@gmail.com", "Muten", "Roshi", "Kame House", "Capsule Corporation", "12:00", "Bulma", "https://pm1.narvii.com/6843/f91922cb75c78054420d8ab9e996cdcec90db821v2_hq.jpg", 1),
+         ("roger@gmail.com", "Gol D.", "Roger", "Logue Town", "Laugh Tale", "11:00", "Rayleigh", "https://www.geekmi.news/__export/1623273715672/sites/debate/img/2021/06/09/rogergold1.jpg_778525087.jpg",1)
+        ]
+    
+    func algo() {
+        print("aqui nomas saludando")
+        db.collection("servicios").getDocuments { (snapshot, err) in
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.algo()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if let controller = segue.destination as? ServicioDetailViewController, let objServicio = sender as? (cliente: String, nombre: String, apellido: String, recojo: String, destino: String, hora: String, conductor: String, imagen: String, estado: Int ) {
+            controller.objServicio = objServicio
+        }
+    }
 }
 
 extension ServicioPorEjecutarViewController: UITableViewDataSource{    
@@ -23,10 +49,16 @@ extension ServicioPorEjecutarViewController: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let objServicio = self.arrayServicios[indexPath.row]
+        self.algo()
         return ServicioTableViewCell.createInTableView(tableView, indexPath: indexPath, objServicio: objServicio)
     }
 }
 
 extension ServicioPorEjecutarViewController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let objServicio = self.arrayServicios[indexPath.row]
+        self.performSegue(withIdentifier: "ServicioDetailViewController", sender: objServicio)
+    }
     
 }
--- proyecto-conductor/proyecto-conductor/Iphone/Classes/Servicio/List/ServicioTableViewCell.swift
+++ proyecto-conductor/proyecto-conductor/Iphone/Classes/Servicio/List/ServicioTableViewCell.swift
@@ -13,19 +13,19 @@ class ServicioTableViewCell: UITableViewCell{
     @IBOutlet private weak var lblLugarDestino: UILabel!
     @IBOutlet private weak var lblHoraRecojo: UILabel!
     
-    var objServicio: String!
+    var objServicio: (cliente: String, nombre: String, apellido: String, recojo: String, destino: String, hora: String, conductor: String, imagen: String, estado: Int )!
     
-    func updateData(_ objServicio:String){
-        self.lblHoraRecojo.text = objServicio
+    func updateData(_ objServicio:(cliente: String, nombre: String, apellido: String, recojo: String, destino: String, hora: String, conductor: String, imagen: String, estado: Int )){
+        self.lblLugarRecojo.text = objServicio.recojo
+        self.lblLugarDestino.text = objServicio.destino
+        self.lblHoraRecojo.text = objServicio.hora
     }
 }
 
 extension ServicioTableViewCell {
-    class func createInTableView(_ tableView:UITableView, indexPath: IndexPath, objServicio:String) -> ServicioTableViewCell {
+    class func createInTableView(_ tableView:UITableView, indexPath: IndexPath, objServicio: (cliente: String, nombre: String, apellido: String, recojo: String, destino: String, hora: String, conductor: String, imagen: String, estado: Int )) -> ServicioTableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ServicioTableViewCell", for: indexPath) as? ServicioTableViewCell
         cell?.updateData(objServicio)
         return cell ?? ServicioTableViewCell()
     }
 }
-
-
